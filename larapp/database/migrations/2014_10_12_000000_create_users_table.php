434b840d0bba4a05c08ebd55370847a032471f24
lauva12 <lvbocanegra@misena.edu.co>
2020-09-08 20:25:50 -0500

    Message

---
+++ larapp/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname');
+            $table->String('email');
+            $table->bigInteger('phone');
+            $table->date('birthdate');
+            $table->string('gender');
+            $table->string('address');
+            $table->string('photo')->default('imgs/no-photo,png');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('role')->default('Customer');
+            $table->boolean('active')->default(1);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ larapp/database/seeds/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'fullname' => 'SaraCatacora',
+            'email' => 'Sara@gmail.net',
+            'phone' => 213432432,
+            'birthdate' => '1988-09-28',
+            'gender' => 'Male',
+            'address' => 'Springfield',
+            'password' => bcrypt('admin'),
+            'role' => 'Admin',
+            'created_at' => now(),
+        ]);
+    }
+}
